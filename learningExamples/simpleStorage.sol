1b0cb644dfaa5d45ca4a33ccf07dd88273101046
Rave <raveggigabriele0@gmail.com>
2022-06-20 16:46:02 +0200

    Simple Storage

---
+++ learningExamples/simpleStorage.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.0;
+
+contract integerStorage {
+    /* Integer data */
+    uint private storedData; // Always initialized to 0
+
+
+    /* storedData setter */
+    function set(uint x) public{
+        storedData = x;
+    }
+
+
+    /* storedData getter */
+    function get() constant public returns (uint){
+        return storedData;
+    } 
+
+
+    /* Increment storedData */
+    function increment(uint n) public {
+        storedData = storedData + n;
+    }
+
+
+    /* Decrement storedData */
+    function decrement(uint n) public {
+        storedData = storedData - n;
+    }
+}
