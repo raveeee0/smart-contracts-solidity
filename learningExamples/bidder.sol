b6c5fc7d814a5cf3224bb75143762717e80feb92
Rave <raveggigabriele0@gmail.com>
2022-06-20 17:37:32 +0200

    Bidder contract

---
+++ learningExamples/bidder.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.0;
+
+contract Bidder {
+
+    /* Fields */
+    string public name;
+    uint public bidAmount;
+    bool public eligible;
+    uint constant minBid = 1000;
+
+    /* name setter */
+    function setName(string nm) public {
+        name = nm;
+    }
+
+    /* bidAmount setter */
+    function setBidAmount(uint x) public {
+        bidAmount = x;
+    }
+
+    /* Check eligibility */
+    function determinaEligibility() public {
+        if(bidAmount >= minBid)
+            eligible = true;
+        else
+            eligible = false;
+    }
+}
