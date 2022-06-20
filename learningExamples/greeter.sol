2bd3ecdc5fa3001a60704de53c0b793b1fa9d486
Rave <raveggigabriele0@gmail.com>
2022-06-20 16:17:50 +0200

    Initial commit

---
+++ learningExamples/greeter.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.0;
+
+contract Greeter {
+    /* Holds your name */
+    string public yourName;
+
+    /* Execution entry point - Initialization */
+    constructor() public {
+        yourName = "World";
+    }
+
+    /* yourName setter */
+    function set(string name) public {
+        yourName = name;
+    }
+
+    function hello() constant public returns (string) {
+        return yourName;
+    }
+
+}
