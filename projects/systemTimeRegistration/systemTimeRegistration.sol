f304dde5d51830a3cf00449c5a46ea2f416d098a
Rave <raveggigabriele0@gmail.com>
2022-06-23 17:06:15 +0200

    Added dependencies and more functions

--- projects/systemTimeRegistration/systemTimeRegistration.sol
+++ projects/systemTimeRegistration/systemTimeRegistration.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.0;
+pragma solidity ^0.8.0;
+
+
+import "https://github.com/bokkypoobah/BokkyPooBahsDateTimeLibrary/blob/master/contracts/BokkyPooBahsDateTimeLibrary.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+
 
 contract Agent {
 
     struct Data {
+        string name;
         uint enterDate;
         uint exitDate;
         bool isIn;
     }
 
     address private coordinator;
-    address[] private addrRegister;
+    address[] private addrRegistered;
     mapping(address => Data) private agents;
 
 
@@ -30,37 +36,72 @@ contract Agent {
     }
 
     function register(address authorized) public onlyCoordinator {
-        authRegister.push(authorized);
+        addrRegistered.push(authorized);
         return;
     }
 
-    function logStatus() view public onlyCoordinator {
-
-    }
-
-    function start(address agentAddress) public {
-
-        bool isAuthorized;
-
-        while(a)
-
-        Agent instance;
-        instance.address
-        instance.isIn = true;
-        instance.startDate = now();
-        agents[msg.sender] = instance;
-    }
-
-    function exit(string identifier) public onlyAuthorized {
+    function registerMultiple(address[] authorized) public onlyCoordinator {
+        for(uint i = 0; i <  authorized.length; i++)
+            addrRegistered.push(authorized[i]);
         
+        return;
     }
 
-    function invoke() public {
-
+    function toFormat(uint year, uint month, uint day, uint hour, uint minute, uint second) pure private returns (string _format) {
+        _format = string.concat(Strings.toString(day), "/", Strings.toString(month), "/", Strings.toString(year), "\t",
+                                                 Strings.toString(hour), ":", Strings.toString(minute), ":", Strings.toString(second));
+                                                
+        return _format;
     }
 
-    function state() view private {
+    function currentStatus() view public onlyCoordinator returns (string status) {
+        uint i = 0;
+        while(i < addrRegistered.length)
+            if(agents[addrRegistered[i]].isIn)
+                status = createCurrentStateString(status, addrRegistered[i], agents[addrRegistered[i]]);
+        return status;
+    }
 
+    function createCurrentStateString(string status, address agentAddress, Agent agent) view private returns (string state) {
+        state = string.concat(status, "\n", string(agentAddress), "\t", agent.name, "\t");
+        
+        string enterDate = toFormat(BokkyPooBahsDateTimeLibrary.timestampToDateTime(agent.enterDate));
+        
+        return string.concat(status, enterDate);
+
+        return state;
+    }
+
+
+    function isAuthorized(address caller) view private returns (bool _bool) {
+        uint i = 0;
+
+        while(i < addrRegistered.length)
+            if(addrRegistered[i] == caller)
+                _bool = true;
+
+        return _bool;
+    }
+
+
+    function start(string calldata name) public {
+        if(isAuthorized(msg.sender) == false)
+            return;
+        agents[msg.sender].name = name;
+        agents[msg.sender].enterDate = now;
+    }
+
+    function exit() public {
+        if(isAuthorized(msg.sender) == false)
+            return;
+
+        if(agents[msg.sender].isIn == false)
+            return;
+        
+        agents[msg.sender].exitDate = now;
+        agents[msg.sender].isIn = false;
+
+        return;
     }
 
 
